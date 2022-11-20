7f530284d99305a3c20cfd93d703609ca0b05033
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-20 10:59:15 +0200

    Init

---
+++ src/main/java/ua/kovalchuk/springvalidationpractice/SpringValidationPracticeApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.springvalidationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/springvalidationpractice/SpringValidationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.springvalidationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

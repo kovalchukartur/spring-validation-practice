00ff4066acb0d38c5047fd6fbef1931b73be854d
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-20 21:22:51 +0200

    Add todos

--- src/test/java/ua/kovalchuk/springvalidationpractice/SpringValidationPracticeApplicationTests.java
+++ src/test/java/ua/kovalchuk/springvalidationpractice/SpringValidationPracticeApplicationTests.java
@@ -3,6 +3,7 @@ package ua.kovalchuk.springvalidationpractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// todo testing empty context need 2-3 sec
 @SpringBootTest
 class SpringValidationPracticeApplicationTests {
 

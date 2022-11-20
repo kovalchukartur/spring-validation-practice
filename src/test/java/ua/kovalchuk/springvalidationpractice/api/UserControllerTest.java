aa71d5a10bec9f7ac0f6865c78dff2ce62c8c453
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-20 14:29:12 +0200

    Add tests for body

--- src/test/java/ua/kovalchuk/springvalidationpractice/api/UserControllerTest.java
+++ src/test/java/ua/kovalchuk/springvalidationpractice/api/UserControllerTest.java
@@ -1,24 +1,18 @@
 package ua.kovalchuk.springvalidationpractice.api;
 
-import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
-import org.springframework.boot.autoconfigure.web.servlet.error.DefaultErrorViewResolver;
-import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
-import org.springframework.web.servlet.mvc.method.annotation.PathVariableMethodArgumentResolver;
 import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
-import ua.kovalchuk.springvalidationpractice.api.error.MethodArgumentNotValidExceptionHandler;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -46,10 +40,40 @@ class UserControllerTest {
         Exception exception = mockMvc.perform(get("/users/test/search"))
             .andDo(print())
             .andExpect(status().isBadRequest())
-            .andReturn()
+           .andReturn()
             .getResolvedException();
 
         assertNotNull(exception);
         assertEquals(MethodArgumentTypeMismatchException.class, exception.getClass());
+    }
+
+    @Test
+    void whenCreateUserNoteByIdIsSuccess() throws Exception {
+        mockMvc.perform(
+                post("/users/1/note")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"name\": \"a\", \"desc\": \"a\"}")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(
+                content().string(containsString("1235"))
+            );
+    }
+
+    @Test
+    void throwExceptionCreateUserNoteByIdAndInvalidName() throws Exception {
+        Exception exception = mockMvc.perform(
+                post("/users/1/note")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"name\": \"\", \"desc\": \"a\"}")
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andReturn()
+            .getResolvedException();
+
+        assertNotNull(exception);
+        assertEquals(MethodArgumentNotValidException.class, exception.getClass());
     }
 }

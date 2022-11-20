23015d62a309dbe798745a95fea6f8379d1edfcc
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-20 15:02:48 +0200

    Add dto parametrize validation

--- src/main/java/ua/kovalchuk/springvalidationpractice/api/dto/NoteDto.java
+++ src/main/java/ua/kovalchuk/springvalidationpractice/api/dto/NoteDto.java
@@ -1,11 +1,17 @@
 package ua.kovalchuk.springvalidationpractice.api.dto;
 
 import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
+@Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class NoteDto {
 
     @NotBlank
--- src/test/java/ua/kovalchuk/springvalidationpractice/api/dto/NoteDtoTest.java
+++ src/test/java/ua/kovalchuk/springvalidationpractice/api/dto/NoteDtoTest.java
@@ -1,32 +1,63 @@
 package ua.kovalchuk.springvalidationpractice.api.dto;
 
 import java.util.Set;
+import java.util.stream.Stream;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class NoteDtoTest {
 
+    public static final String NAME_PATH = "name";
+    public static final String DESC_PATH = "desc";
+    public static final String VALID_VALUE = "desc_value";
     private Validator validator;
+
+    public static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+            Arguments.of(null, VALID_VALUE, NAME_PATH),
+            Arguments.of("", VALID_VALUE, NAME_PATH),
+            Arguments.of("  ", VALID_VALUE, NAME_PATH),
+            Arguments.of(VALID_VALUE, null, DESC_PATH),
+            Arguments.of(VALID_VALUE, "  ", DESC_PATH),
+            Arguments.of(VALID_VALUE, "  ", DESC_PATH)
+        );
+    }
 
     @BeforeEach
     public void setUp() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
     }
 
-    @Test
-    void returnErrorWhenValidateNoteDto() {
-        NoteDto noteDto = new NoteDto();
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")
+    void returnErrorWhenValidateNoteDto(String nameValue, String descValue, String propertyPath) {
+        NoteDto noteDto = buildNoteDto(nameValue, descValue);
         Set<ConstraintViolation<NoteDto>> constraintViolations = validator.validate(noteDto);
 
         assertNotNull(constraintViolations);
         assertFalse(constraintViolations.isEmpty());
+        assertEquals(1, constraintViolations.size());
+        ConstraintViolation<NoteDto> constraintViolation = constraintViolations.iterator().next();
+        assertEquals("must not be blank", constraintViolation.getMessage());
+        assertEquals(propertyPath, constraintViolation.getPropertyPath().toString());
+    }
+
+    private NoteDto buildNoteDto(String name, String desc) {
+        return NoteDto.builder()
+            .name(name)
+            .desc(desc)
+            .build();
     }
 }
